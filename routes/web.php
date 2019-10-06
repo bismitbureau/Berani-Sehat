4ceda59fc2542110259ef3220f97380500766ea8
Unknown <dhafins18@gmail.com>
2019-10-06 14:00:09 +0700

    Add all backend features

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'BlogController@index');
+Route::get('/posts/{post}', 'BlogController@post');
+Route::post('/posts/{post}/comment', 'BlogController@comment')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function() {
+    Route::resource('/posts', 'PostController');
+    Route::put('/posts/{post}/publish', 'PostController@publish')->middleware('admin');
+    Route::resource('/categories', 'CategoryController', ['except' => ['show']]);
+    Route::resource('/tags', 'TagController', ['except' => ['show']]);
+    Route::resource('/comments', 'CommentController', ['only' => ['index', 'destroy']]);
+    Route::resource('/users', 'UserController', ['middleware' => 'admin', 'only' => ['index', 'destroy']]);
 });
